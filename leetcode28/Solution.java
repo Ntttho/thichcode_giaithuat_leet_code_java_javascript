f67f4611fa0f466283e139cd41b2244a1f91a690
Ntttho <nguyentatho14106@gmail.com>
2026-07-09 07:14:58 +0700

    leetcode28

---
+++ leetcode28/Solution.java
@@ -0,0 +1,20 @@
+package leetcode28;
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int i = 0, length = needle.length();
+        for(;length <= haystack.length(); i++, length++){
+            System.out.println( haystack.subSequence(i, length));
+            if(haystack.subSequence(i, length).equals(needle)){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int i = solution.strStr("sadbutsad", "sad");
+        System.out.println(i);
+    }
+}
