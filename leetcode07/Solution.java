69714a9cf6aaee22fe2255f3d3b57dba7a897ae9
Ntttho <nguyentatho14106@gmail.com>
2026-08-04 17:13:44 +0700

    leetcode83-leetcode82-leetcode07-100%beats-oh-damm

--- leetcode07/Solution.java
+++ leetcode07/Solution.java
@@ -3,12 +3,16 @@ package leetcode07;
 public class Solution {
 
     public int reverse(int x) {
-        int result = 0;
+        long result = 0;
         while (x != 0) {
             result = result * 10 + x % 10;
             x = x / 10;
         }
-        return result;
+        // incase x = 1534236469 // that is crazy for the size of this dataType
+        if(result > Integer.MAX_VALUE || result<Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)result;
     }
 
     public static void main(String[] args) {
