392224bc2c91aad10698c5721807d882fcfee205
Ntttho <nguyentatho14106@gmail.com>
2026-09-26 23:35:18 +0700

    dp-countbits

--- java_leetcode/dynamic_program/leetcode338/Solution.java
+++ java_leetcode/dynamic_program/leetcode338/Solution.java
@@ -1,18 +1,34 @@
 package java_leetcode.dynamic_program.leetcode338;
 
+import java.util.List;
+
 class Solution {
     public int[] countBits(int n) {
-        int[] dp = new int[n + 1];
+        int dp[] = new int[n + 1];
         int sub = 1;
-
-        for (int i = 1; i <= n; i++) {
+        for(int i = 0; i <= n; i++){
             if (sub * 2 == i) {
                 sub = i;
+            }else{
+                dp[i] = dp[i - sub] + 1;
             }
-
-            dp[i] = dp[i - sub] + 1;
         }
 
-        return dp;        
+
+        return dp;
     }
 }
+
+/*
+i   byte    res
+0   0   -> 0
+1   1   -> 1
+2   10  -> 1
+3   11  -> 2
+4   100 -> 1 (reset) (res[0] + 1)
+5   101 -> 2    (res[1] + 1) <1>
+6   110 -> 2    (res[2] + 1) <2>  => từ <1> và <2> suy ra res[4] cái trên là
+7   111 -> 3    res[3] + 1
+8   tương tự
+9
+*/
